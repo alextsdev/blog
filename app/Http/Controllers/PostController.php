acbfd514223d8fc6b87212f6b401bdfc13b86e74
Alex_dev <131791659+SpeedAlex150@users.noreply.github.com>
2024-10-15 16:48:00 +0200

    Refactorizacion de codigo PostController en Store/UpdateRequest

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -26,22 +28,12 @@ class PostController extends Controller
         ]);
     }
 
-    public function store(Request $request)
+    public function store(StorePostRequest $request)
     {
-        $request->validate([
-            'title' => 'required|min:5',
-            'body' => 'required',
-        ],
-        );
+        Post::create($request->validated());
 
-       $post = new Post();
-       $post->title = $request->input('title');
-       $post->body = $request->input('body');
-       $post->save();
-
-       session()->flash('status', 'Post created successfully');
-
-       return redirect()->route('posts.index');
+        return to_route('posts.index')
+            ->with('status', 'Post created successfully');
     }
 
     public function edit(Post $post)
@@ -49,20 +41,12 @@ class PostController extends Controller
         return view('posts.edit', compact('post'));
     }
 
-    public function update(Request $request, Post $post)
+    public function update(UpdatePostRequest $request, Post $post)
     {
-        $request->validate([
-            'title' => 'required|min:5',
-            'body' => 'required',
-        ],
-        );
 
-        $post->title = $request -> input('title');
-        $post->body = $request -> input('body');
-        $post->save();
+        $post->update($request->validated());
 
-        session()->flash('status', 'Post updated successfully');
-
-        return to_route('posts.show', $post);
+        return to_route('posts.show', $post)
+            ->with('status', 'Post updated successfully');
     }
 }
