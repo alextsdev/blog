45c61486dde96a14594a2828dd7d6747573a49aa
Alex_dev <131791659+SpeedAlex150@users.noreply.github.com>
2024-10-18 15:59:21 +0200

    Activar la verificacion desde el modelo User

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,6 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::view('/', 'welcome')->name('home');
-Route::view('contacto','contact')->name('contact');
-Route::resource('blog', PostController::class)
-    ->names('posts')
-    ->parameters(['blog' => 'post']);
-
 /**
  * Esta linea sustituye el CRUD estandar de laravel
  * Route::resource('blog', PostController::class)->names('posts')->parameters(['blog' => 'post']);
@@ -23,6 +16,13 @@ Route::resource('blog', PostController::class)
  * Route::patch('blog/{post}', [PostController::class, 'update'])->name('posts.update');
  * Route::delete('blog/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
  */
+
+
+Route::view('/', 'welcome')->name('home');
+Route::view('contacto','contact')->name('contact');
+Route::resource('blog', PostController::class)
+    ->names('posts')
+    ->parameters(['blog' => 'post']);
 
 Route::view('nosotros','about')->name('about');
 
