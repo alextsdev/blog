198ee1830bb161deb69acc6c1bbc3d4b0a043ade
Alex_dev <131791659+SpeedAlex150@users.noreply.github.com>
2024-10-22 16:38:23 +0200

    Refactorizar componentes y ajustar estilos en la creacion y edicion de post

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<x-layout meta-title="Inicio" meta-description="Descripción de la página de Inicio">
+<x-blog-layout meta-title="Inicio" meta-description="Descripción de la página de Inicio">
     <div class="mx-auto mt-4 max-w-6xl">
         <h1 class="mt-4 mb-8 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
             Welcome
@@ -7,4 +7,4 @@
     <x-slot name="sidebar"> <!--x-slot:sidebar (otra forma de hacer lo mismo)-->
         <p>Home Sidebar</p>
     </x-slot>
-</x-layout>
+</x-blog-layout>
